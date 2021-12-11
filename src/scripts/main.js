471e2c9b7482c3b6193a516c5d01e46c47c3e426
JacobF94 <jacob.frensley@gmail.com>
2021-12-11 15:26:38 -0600

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+import { SinkRepair } from "./SinkRepair.js"
+
+
+const mainContainer = document.querySelector("#container")
+
+const render = () => {
+    mainContainer.innerHTML = SinkRepair()
+}
+
+render()
